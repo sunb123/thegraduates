3410b9fef24d4597b7c4f5d06e69f4cb6bcb220f
Marco Cusumano-Towner <imarcoam@gmail.com>
2016-04-13 23:35:40 -0400

    Posted events table and buttons

---
+++ src/js/activity.js
@@ -0,0 +1,8 @@
+function Activity(locstr, dist, datestr, num, difficulty) {
+    this.locstr = locstr;
+    this.dist = dist;
+    this.datestr = datestr;
+    this.num = num;
+    this.difficulty = difficulty;
+    
+};
