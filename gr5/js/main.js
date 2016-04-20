b8f20441f216346f883629e166658b3b50b6c83a
kevin qi <kevinkeyjkw@gmail.com>
2016-04-20 17:23:02 -0400

    Shared code for modal, navbar

---
+++ gr5/js/main.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Kevin on 4/20/16.
+ */
+
+$(document).ready(function(){
+
+    $("#nav-placeholder").load('nav.html');
+    $("#modal-placeholder").load('createEventModal.html');
+
+});
